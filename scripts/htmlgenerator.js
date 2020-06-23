9664dddbef0bd640626f799b8482fa77b0aa3936
Josh Struve <jstruve@gmail.com>
2020-06-22 19:22:28 -0700

    ally updates

--- scripts/htmlgenerator.js
+++ scripts/htmlgenerator.js
@@ -8,7 +8,8 @@ function generateHomeScreen(filteredBookmarks) {
   <section class="flex-group-column">
   <form id="add-filter">
       <ul class="flex-item-buttons-top">
-    <select name="star-filter" id="stars" class="home-stars">
+    <label for="stars" class="hidden">Filter</label>
+      <select name="star-filter" id="stars" class="home-stars">
       <option class="home-stars" value="-1">Filter </option>
       <option class="home-stars" value="1">1+ Star</option>
       <option class="home-stars" value="2">2+ Stars</option>
@@ -82,24 +83,27 @@ function generateAddScreen() {
   <section class="raised-card">
   <section class="lowered-card">
   <form>
+  <fieldset>
+  <legend class="hidden">Add a Bookmark</legend>
       <label for="bookmark-name">Title</label>
       <input id="bookmark-name" class="form-control" type="text" placeholder="Title" required />
       <label for="url">URL</label>
       <input id="url" class="form-control" type="url" placeholder="https://example.com" pattern="https://.*" required />
       <label for="desc">Description</label>
       <input type="textarea" id="desc" name="description" class="form-control" placeholder="Description" required>
-      <label for="star-rating-in-form">Rating</label>
+      <section>Rating</section>
       <section class="star-rating-in-form">
-      <input type="radio" id="star" name="rating" value="5" checked/><label for="star5">5 stars</label>
-      <input type="radio" id="star" name="rating" value="4" /><label for="star4">4 stars</label>
-      <input type="radio" id="star" name="rating" value="3" /><label for="star3">3 stars</label>
-      <input type="radio" id="star" name="rating" value="2" /><label for="star2">2 stars</label>
-      <input type="radio" id="star" name="rating" value="1" /><label for="star1">1 star</label>
+      <input type="radio" id="star5" name="rating" value="5" /><label for="star5">5 stars</label>
+      <input type="radio" id="star4" name="rating" value="4" /><label for="star4">4 stars</label>
+      <input type="radio" id="star3" name="rating" value="3" /><label for="star3">3 stars</label>
+      <input type="radio" id="star2" name="rating" value="2" /><label for="star2">2 stars</label>
+      <input type="radio" id="star1" name="rating" value="1" /><label for="star1">1 star</label>
       </section>
         <section class="flex-item-buttons-inner">
         <button class="button-danger" type="click" id="cancel">Cancel</button>  
-        <input class="button-primary" type="submit" id="add-bookmark" required></input>
+        <input class="button-primary" type="submit" id="add-bookmark" value="submit" required></input>
         </section>
+        </fieldset>
     </form>
     </section>
     </section>
